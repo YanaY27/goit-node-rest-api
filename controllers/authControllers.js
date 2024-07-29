9e76956742d5c6af9bd8885f8d0ee7fba7a503d3
YanaY27 <yanayeromina@gmail.com>
2024-07-29 20:01:02 +0300

    upd

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,78 +1,97 @@
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+import path from "path";
+import fs from "fs/promises";
+import { fileURLToPath } from "url";
+import gravatar from "gravatar";
+import jimp from "jimp";
 import User from "../models/user.js";
 import { HttpError } from "../helpers/HttpError.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
-const { JWT_SECRET } = process.env;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const avatarsDir = path.join(__dirname, "../public/avatars");
 
 export const register = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-
-    if (user) {
-      throw HttpError(409, "Email in use");
-    }
-
-    const hashPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({ email, password: hashPassword });
-
-    res.status(201).json({
-      user: {
-        email: newUser.email,
-        subscription: newUser.subscription,
-      },
-    });
-  } catch (error) {
-    next(error);
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    next(HttpError(409, "Email in use"));
   }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email, { s: "250", d: "identicon" });
+
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
+  res.status(201).json({
+    user: {
+      email: newUser.email,
+      subscription: newUser.subscription,
+      avatarURL: newUser.avatarURL,
+    },
+  });
 };
 
 export const login = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      throw HttpError(401, "Email or password is wrong");
-    }
-
-    const payload = { id: user._id };
-    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
-
-    await User.findByIdAndUpdate(user._id, { token });
-
-    res.json({
-      token,
-      user: {
-        email: user.email,
-        subscription: user.subscription,
-      },
-    });
-  } catch (error) {
-    next(error);
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    next(HttpError(401, "Email or password is wrong"));
   }
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    next(HttpError(401, "Email or password is wrong"));
+  }
+
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
+  await User.findByIdAndUpdate(user._id, { token });
+
+  res.json({
+    token,
+    user: {
+      email: user.email,
+      subscription: user.subscription,
+    },
+  });
 };
 
 export const logout = async (req, res, next) => {
-  try {
-    const { _id } = req.user;
-    await User.findByIdAndUpdate(_id, { token: null });
-
-    res.status(204).send();
-  } catch (error) {
-    next(error);
-  }
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: null });
+  res.status(204).json();
 };
 
 export const getCurrentUser = async (req, res, next) => {
-  try {
-    const { email, subscription } = req.user;
-    res.json({
-      email,
-      subscription,
-    });
-  } catch (error) {
-    next(error);
-  }
+  const { email, subscription } = req.user;
+  res.json({
+    email,
+    subscription,
+  });
+};
+
+export const updateAvatar = async (req, res, next) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const filename = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, filename);
+
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatars", filename);
+
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  const image = await jimp.read(resultUpload);
+  await image.resize(250, 250).write(resultUpload);
+
+  res.json({ avatarURL });
 };
---
+++ middleware/upload.js
@@ -0,0 +1,22 @@
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const tmpDir = path.join(__dirname, "../tmp");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, tmpDir);
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+export default upload;
